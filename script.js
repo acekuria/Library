45a5307433ae91a0daf2f8d046d345a808170ac1
AlvinKuria <alvinkuria18@gmail.com>
2023-05-27 15:27:17 +0100

    Form before styling done

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+
 let myLibrary = [];
 
-
+let title = document.querySelector('title').textContent;
+let author = document.querySelector('author').textContent;
+let noOfPages = document.querySelector('noOfPages').textContent;
+let isRead = document.querySelector('isRead').textContent;
+let submitButton = document.querySelector('submit');
 
 function Book (title, author, noOfPages, isRead) {
   this.title = title;
@@ -9,12 +14,17 @@ function Book (title, author, noOfPages, isRead) {
   this.isRead = isRead;
 }
 
-Book.prototype.info = function () {
-  return `The ${this.title} by ${this.author}, ${this.noOfPages}, ${this.isRead} `
-}
+submitButton.addEventListener('click', function (e) {
+  let book = new Book(this.title, this.author, this.noOfPages, this.isRead)
+})
+
+
+// Book.prototype.info = function () {
+  // return `The ${this.title} by ${this.author}, ${this.noOfPages}, ${this.isRead} `
+// }
 
 
 
 function addBookToLibrary () {
-
+  return myLibrary += 
 }
